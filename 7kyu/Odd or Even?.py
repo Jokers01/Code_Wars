0ad8f1a9e52a90784fb176ad6ef382d943a0c9ec
Jokers01 <jokerszero01@gmail.com>
2018-05-11 10:40:03 +0700

    7kyu

---
+++ 7kyu/Odd or Even?.py
@@ -0,0 +1,28 @@
+"""
+Task:
+
+Given an array of numbers (a list in groovy), determine whether the sum of all of the numbers is odd or even.
+
+Give your answer in string format as 'odd' or 'even'.
+
+If the input array is empty consider it as: [0] (array with a zero).
+Example:
+
+odd_or_even({0}, 1) returns "even"
+odd_or_even({2, 5, 34, 6}, 4) returns "odd"
+odd_or_even({0, -1, -5}, 3) returns "even"
+
+Have fun!
+"""
+
+#answer
+def oddOrEven(arr):
+    s = sum(arr)
+    if s % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+#or
+def oddOrEven(arr):
+    return 'even' if sum(arr) % 2 == 0 else 'odd' 
---
+++ 7kyu/lucky number.py
@@ -0,0 +1,22 @@
+"""
+###Lucky number
+
+Write a function to find if a number is lucky or not. If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisble by 9, hence number is lucky.
+
+Function will return true for lucky numbers and false for others.
+
+"""
+
+
+#answer
+
+def is_lucky(n):
+    try:
+        if n % 9 == 0:
+            return True
+        else:
+            return False
+    except:
+        raise Exception("TODO: is_lucky") 
