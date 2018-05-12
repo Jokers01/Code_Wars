2355d369e0728738efe156e7a466940329d4e11a
Jokers01 <jokerszero01@gmail.com>
2018-05-12 21:23:18 +0700

    8kyu

---
+++ 8kyu/Function 3 - multiplying two numbers.py
@@ -0,0 +1,12 @@
+"""
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+"""
+
+
+
+#answer
+def multiply(x,y):
+    return x * y
+
+
+multiply(10, 20)
